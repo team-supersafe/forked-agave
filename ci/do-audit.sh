badf555942714610793925616d843a54a041ce81
Trent Nelson <490004+t-nelson@users.noreply.github.com>
2026-02-04 17:16:35 -0700

    V3.1 ignore byte audit (#10391)
    * ignore bytes security audit
    
    * ci - prevent introduction of ignored RUSTSEC-2026-0007 vulnerabilities
    
    ---------
    
    Co-authored-by: yihau <yihau.chen@icloud.com>

--- ci/do-audit.sh
+++ ci/do-audit.sh
@@ -59,6 +59,15 @@ cargo_audit_ignores=(
   # URL:       https://rustsec.org/advisories/RUSTSEC-2024-0376
   # Solution:  Upgrade to >=0.12.3
   --ignore RUSTSEC-2024-0376
+
+	# Crate:     bytes
+	# Version:   1.10.1
+	# Title:     Integer overflow in `BytesMut::reserve`
+	# Date:      2026-02-03
+	# ID:        RUSTSEC-2026-0007
+	# URL:       https://github.com/advisories/GHSA-434x-w66g-qw3r
+	# Solution:  Upgrade to >=1.11.1
+	--ignore RUSTSEC-2026-0007
 )
 scripts/cargo-for-all-lock-files.sh audit "${cargo_audit_ignores[@]}" | $dep_tree_filter
 # we want the `cargo audit` exit code, not `$dep_tree_filter`'s
